0fd1a575f8335013cef9d9cd22647ac1ce6532ed
Adityavikrambhatta <adi95vb@gmail.com>
2024-03-09 13:44:54 +0530

    Setting up basic express app

--- app.js
+++ app.js
@@ -4,25 +4,11 @@ const port = 3000;
 const path = require('path')
 require('dotenv').config();
 
-// const {signUp, login} = require(path.join(__dirname,"src", "routers", "authRouter.js"))
-// const newsActions = require(path.join(__dirname,"src", "routers", "newsActionsRouters.js"))
-// const verifyToken = require(path.join(__dirname,"src","routers","middleware.js"));
-
-// const mongoose = require('mongoose');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// try{
-//     mongoose.connect("mongodb://localhost:27017/userDb",{
-//             useUnifiedTopology: true,
-//             useNewUrlParser: true
-//     });
-//     console.log("Connected to the DB")
 
-// }catch(err){
-//     console.log("Connection to the DB failed")
-// }
 
 app.get('/', (req, res)=>{
     return res.status(200).send("<HTML><h1>Server  Initialised . <br/> Welcome to 'Virtual Event Management'  </h1></HTML>")
